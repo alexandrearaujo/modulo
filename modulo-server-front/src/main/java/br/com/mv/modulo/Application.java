7ef56f50076057767024d215ff48eca65498d71b
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-05-13 09:36:43 -0300

    fix config for entity scan

--- modulo-server-front/src/main/java/br/com/mv/modulo/Application.java
+++ modulo-server-front/src/main/java/br/com/mv/modulo/Application.java
@@ -3,12 +3,8 @@ package br.com.mv.modulo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
-@EntityScan({"org.springframework.data.jpa.domain.support", "br.com.mv.geral.model", "br.com.mv.modulo.model"})
 public class Application extends WebMvcAutoConfigurationAdapter {
 
     public static void main(String[] args) {
