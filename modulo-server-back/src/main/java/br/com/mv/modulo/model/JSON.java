3c8a900ed556a75c2b6061a2c79dbc2f863a8e31
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-06-03 10:26:31 -0300

    knockout improvements

---
+++ modulo-server-back/src/main/java/br/com/mv/modulo/model/JSON.java
@@ -0,0 +1,29 @@
+package br.com.mv.modulo.model;
+
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public interface JSON {
+
+	@Transient
+	public default String toJSON() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY,true);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		String json = "";
+		try {
+			json = mapper.writeValueAsString(this);
+		} catch (JsonProcessingException e) {
+			json = "{ error : Error: it's not possible processing JSON }";
+		}
+		return json;
+	}
+
+}
