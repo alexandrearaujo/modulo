ed29dec5479bb64cae9b0f4c59ff1112e4172c52
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-06-03 09:57:56 -0300

    adding awesome-bootstrap-checkbox and removing iCheck. test npm config

--- modulo-client/src/components/selectable.js
+++ modulo-client/src/components/selectable.js
@@ -1,30 +1,3 @@
-/**
- * Checkbox-Field
- */
-function initCheckboxField(){
-	var targets = $('.checkbox-field');
-	jQuery.each(targets, function(i, target){ 
-		if( target !== undefined){
-			var jQueryTarget = $(target);
-			jQueryTarget.iCheck({ checkboxClass: 'icheckbox_minimal-blue' });
-			jQueryTarget.on('ifChanged', function(){
-				var functionName = jQueryTarget.attr('data-onchange');
-				var f = new Function(functionName);
-				f.call();
-			});
-		}
-	});
-}
-
-/**
- * Radio-Field
- */
-function initRadioField(){
-	$('.radio-field').iCheck({
-   		radioClass: 'iradio_minimal-blue radio'
-  	});
-}
-
 function fncPopulaCombo(vUrl, vElemento){
 	var vIdentificador = vElemento + 'Id';
 	$.post(vUrl, function(data) {
