8f6728819eda74fc6407806d383cd0ba9dfae532
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-06-15 17:30:37 -0300

    arquitetura

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y zlib1g-dev
 sudo apt-get install -y ruby-full
 sudo gem install jekyll
-sudo gem install jekyll-mentions
+sudo gem isntall redcarpet
 sudo gem install jemoji
 sudo gem install jekyll-redirect-from
 sudo gem install jekyll-sitemap
