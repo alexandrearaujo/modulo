59f3ed538ba20a62168d097fded72ea00a166183
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-07-06 09:52:17 -0300

    added WebConfig class, including json and messages configuration

--- modulo-server-front/src/main/java/br/com/mv/modulo/Application.java
+++ modulo-server-front/src/main/java/br/com/mv/modulo/Application.java
@@ -2,10 +2,9 @@ package br.com.mv.modulo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 
 @SpringBootApplication
-public class Application extends WebMvcAutoConfigurationAdapter {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
