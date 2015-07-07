bad83db3ca7da2565272f51fa432eea581688d11
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-07-07 10:13:47 -0300

    polishing converters

--- modulo-server-front/src/main/java/br/com/mv/modulo/WebConfig.java
+++ modulo-server-front/src/main/java/br/com/mv/modulo/WebConfig.java
@@ -4,22 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
 
-import javax.xml.transform.Source;
-
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.ResourceHttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
-import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -41,6 +34,7 @@ public class WebConfig extends WebMvcAutoConfigurationAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+		super.addInterceptors(registry);
 	}
 	
 	@Bean
@@ -63,26 +57,18 @@ public class WebConfig extends WebMvcAutoConfigurationAdapter {
 	
 	@Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
-        stringConverter.setWriteAcceptCharset(false);
-        converters.add(stringConverter);
-
-        converters.add(new ByteArrayHttpMessageConverter());
-        converters.add(new ResourceHttpMessageConverter());
-        converters.add(new SourceHttpMessageConverter<Source>());
-        converters.add(new AllEncompassingFormHttpMessageConverter());
-
-        converters.add(jackson2Converter());
+        converters.add(mappingJackson2HttpMessageConverter());
+        super.configureMessageConverters(converters);
     }
 
     @Bean
-    public MappingJackson2HttpMessageConverter jackson2Converter() {
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(jacksonBuilder().build());
         return converter;
     }
     
-	public Jackson2ObjectMapperBuilder jacksonBuilder() {
+	private Jackson2ObjectMapperBuilder jacksonBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         builder.modulesToInstall(new Hibernate4Module().disable(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION));
         builder.indentOutput(true).dateFormat(new SimpleDateFormat("dd/MM/yyyy"));
