d7fbd5e50479a6db1eab7ac64dd2080bc5b8457c
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-09-09 11:45:05 -0300

    temporarily removing modulo tests

--- modulo-tests/src/main/java/br/com/mv/modulo/ModuloWebDriverTestsConfig.java
+++
@@ -1,8 +0,0 @@
-package br.com.mv.modulo;
-
-import org.springframework.boot.test.SpringApplicationConfiguration;
-
-@SpringApplicationConfiguration(classes = Application.class)
-public abstract class ModuloWebDriverTestsConfig extends WebDriverTestsConfig {
-
-}
