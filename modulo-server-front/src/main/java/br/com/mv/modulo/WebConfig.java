a558ca1efcfafebe2da4cdf12865d5f7ba166c49
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2015-12-30 16:48:21 -0300

    atualização com novas configurações do spring boot 1.3

--- modulo-server-front/src/main/java/br/com/mv/modulo/WebConfig.java
+++ modulo-server-front/src/main/java/br/com/mv/modulo/WebConfig.java
@@ -4,8 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -17,70 +15,16 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.resource.ContentVersionStrategy;
-import org.springframework.web.servlet.resource.VersionResourceResolver;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
-
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 @EnableSpringDataWebSupport
 @Configuration
-@Slf4j
 public class WebConfig extends WebMvcAutoConfigurationAdapter {
-	
-	protected static final String[] SERVLET_RESOURCE_LOCATIONS = { "/" };
-	
-	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-			"classpath:/META-INF/resources/", "classpath:/resources/",
-			"classpath:/static/", "classpath:/public/" };
-	
-	private static final String[] RESOURCE_LOCATIONS;
-
-	static {
-		RESOURCE_LOCATIONS = new String[CLASSPATH_RESOURCE_LOCATIONS.length
-				+ SERVLET_RESOURCE_LOCATIONS.length];
-		System.arraycopy(SERVLET_RESOURCE_LOCATIONS, 0, RESOURCE_LOCATIONS, 0,
-				SERVLET_RESOURCE_LOCATIONS.length);
-		System.arraycopy(CLASSPATH_RESOURCE_LOCATIONS, 0, RESOURCE_LOCATIONS,
-				SERVLET_RESOURCE_LOCATIONS.length, CLASSPATH_RESOURCE_LOCATIONS.length);
-	}
-	
-	
-	@Autowired
-	private ResourceProperties resourceProperties;
-
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
-				.addVersionStrategy(new ContentVersionStrategy(), "/**");
-		
-		if (!this.resourceProperties.isAddMappings()) {
-			log.debug("Default resource handling disabled");
-			return;
-		}
-
-		Integer cachePeriod = this.resourceProperties.getCachePeriod();
-		if (!registry.hasMappingForPattern("/webjars/**")) {
-			registry.addResourceHandler("/webjars/**")
-					.addResourceLocations("classpath:/META-INF/resources/webjars/")
-					.setCachePeriod(cachePeriod)
-					.resourceChain(true)
-					.addResolver(versionResourceResolver);
-		}
-		
-		if (!registry.hasMappingForPattern("/**")) {
-			registry.addResourceHandler("/**")
-					.addResourceLocations(RESOURCE_LOCATIONS)
-					.setCachePeriod(cachePeriod)
-					.resourceChain(true)
-					.addResolver(versionResourceResolver);
-		}
-	}
 	
 	@Bean
 	@Override
@@ -116,10 +60,9 @@ public class WebConfig extends WebMvcAutoConfigurationAdapter {
 	}
 	
 	@Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(mappingJackson2HttpMessageConverter());
-        super.configureMessageConverters(converters);
-    }
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(mappingJackson2HttpMessageConverter());
+	}
 
     @Bean
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
@@ -130,8 +73,12 @@ public class WebConfig extends WebMvcAutoConfigurationAdapter {
     
 	private Jackson2ObjectMapperBuilder jacksonBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.modulesToInstall(new Hibernate4Module().disable(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION));
-        builder.indentOutput(true).dateFormat(new SimpleDateFormat("dd/MM/yyyy"));
+        builder
+        	.indentOutput(true)
+        	.dateFormat(new SimpleDateFormat("dd/MM/yyyy"))
+        	.modulesToInstall(
+        			new Hibernate4Module().disable(Hibernate4Module.Feature.USE_TRANSIENT_ANNOTATION),
+        			new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
         return builder;
     }
 }
