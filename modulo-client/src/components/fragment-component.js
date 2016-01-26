b72ea82f81bcda0369a864902b2fd708d08c03ea
Mozart Militão <francisco.freire@mv.com.br>
2016-01-26 18:02:23 -0300

    Implementação componente autocomplete

--- modulo-client/src/components/fragment-component.js
+++ modulo-client/src/components/fragment-component.js
@@ -33,3 +33,33 @@ ko.components.register('label-field', {
     template:
     	'<label data-bind="css: cssClass, id : idLabel, attr: { for : idField }, text: label"><span ></span></label>'	    
 });
+
+ko.components.register('mv-autocomplete',{
+			viewModel : function(params, componentInfo) {
+				this.label = params.label;
+				this.id = params.id;
+				this.idLabel = params.id + 'Label';
+				this.required = params.required;
+				this.value = params.value;
+				this.valueText = params.valueText;
+				this.optionsValue = params.optionsValue || {};
+				this.optionsText = params.optionsText || {};
+				this.optionsLabel = params.optionsLabel || {};
+				this.disabled = params.disabled;
+				this.source = params.source;
+				this.params = params.params || function() {
+					return []
+				};
+				this.otherValues = params.otherValues;
+			},
+			template : '<div class="form-group">\
+							<label-field params = "idLabel : idLabel, label : label, idField : id, required : required "></label-field>\
+							<input class="typeahead" type="text"\
+												data-bind="mvautocomplete: { source: source,\
+												optionsValue: optionsValue, optionsText: optionsText, \
+												optionsLabel: optionsLabel , \
+												value: value, valueText: valueText, \
+										    	params: params(), \
+												otherValues: otherValues}" /> \
+						</div>'
+});
