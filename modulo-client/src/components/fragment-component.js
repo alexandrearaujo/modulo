f96540f54575d08bb2328ace45ef5e09d193526c
Dácio Barros Tavares <daciot@gmail.com>
2016-02-03 11:27:16 -0300

    Implementação do periodo

--- modulo-client/src/components/fragment-component.js
+++ modulo-client/src/components/fragment-component.js
@@ -1,16 +1,23 @@
+
+function ViewModelGenericComponent(params){
+	this.params = params;
+	this.params.id = params.id || getHashId();
+	this.idLabel = this.params.id + 'Label'; 
+}
+
 ko.components.register('mv-date', {
     viewModel: function(params) {
-    	this.params = params;
-        this.idLabel = params.id + 'Label';
-        this.idIcon = params.id + 'Icon';
+    	//Herança
+    	ViewModelGenericComponent.call(this, params);
+    	
         this.options = params.options || {};
     },
     template:
-    	'<div class="date-field form-group" data-bind="mvdate: options, value: params.value, attr: {id: params.id, disabled : params.disabled}">\
+    	'<div class="date-field date form-group" data-bind="mvDate: options, value: params.value, attr: {disabled : params.disabled}">\
     		<label-field params = "idLabel : idLabel, label : params.label, idField : params.id, required : params.required "></label-field>\
-	        <div class="input-group date input-date-field date-field">\
-    			<input type="text" class="form-control" data-bind = "attr : { id : params.id}" />\
-		    	<span data-bind="attr : {id : idIcon, disabled : params.disabled}" class="input-group-addon btn">\
+	        <div class="input-group  input-date-field date-field">\
+    			<input type="text" class="form-control" data-bind = "attr: {id: params.id, disabled : params.disabled}" />\
+		    	<span data-bind="attr : {disabled : params.disabled}" class="input-group-addon btn">\
 		    		<span class="glyphicon glyphicon-calendar"></span>\
 		    	</span>\
 	        </div>\
@@ -31,31 +38,60 @@ ko.components.register('label-field', {
 });
 
 ko.components.register('mv-autocomplete',{
-			viewModel : function(params) {
-				this.label = params.label;
-				this.id = params.id;
-				this.idLabel = params.id + 'Label';
-				this.required = params.required;
-				this.value = params.value;
-				this.valueText = params.valueText;
-				this.optionsValue = params.optionsValue || {};
-				this.optionsText = params.optionsText || {};
-				this.optionsLabel = params.optionsLabel || {};
-				this.disabled = params.disabled;
-				this.source = params.source;
-				this.params = params.params || function() {
-					return []
-				};
-				this.otherValues = params.otherValues;
-			},
-			template : '<div class="form-group">\
-							<label-field params = "idLabel : idLabel, label : params.label, idField : params.id, required : params.required "></label-field>\
-							<input class="typeahead" type="text"\
-												data-bind="mvautocomplete: { source: source,\
-												optionsValue: optionsValue, optionsText: optionsText, \
-												optionsLabel: optionsLabel , \
-												value: value, valueText: valueText, \
-										    	params: params(), \
-												otherValues: otherValues}" /> \
-						</div>'
+	viewModel : function(params) {
+		this.label = params.label;
+		this.id = params.id;
+		this.idLabel = params.id + 'Label';
+		this.required = params.required;
+		this.value = params.value;
+		this.valueText = params.valueText;
+		this.optionsValue = params.optionsValue || {};
+		this.optionsText = params.optionsText || {};
+		this.optionsLabel = params.optionsLabel || {};
+		this.disabled = params.disabled;
+		this.source = params.source;
+		this.params = params.params || function() {
+			return []
+		};
+		this.otherValues = params.otherValues;
+	},
+		template : '<div class="form-group">\
+						<label-field params = "idLabel : idLabel, label : params.label, idField : params.id, required : params.required "></label-field>\
+						<input class="typeahead" type="text"\
+											data-bind="mvautocomplete: { source: source,\
+											optionsValue: optionsValue, optionsText: optionsText, \
+											optionsLabel: optionsLabel , \
+											value: value, valueText: valueText, \
+									    	params: params(), \
+											otherValues: otherValues}" /> \
+					</div>'
+});
+
+ko.components.register('mv-period', {
+	viewModel: function(params) {
+		//Herança
+    	ViewModelGenericComponent.call(this, params);
+    	
+		this.startDate = params.startDate || {};
+		this.endDate = params.endDate || {};
+	},
+	template:
+		'<div class="date-field-periodo form-group ">\
+			<label-field params = "idLabel : idLabel, label : params.label, idField : startDate.id, required : startDate.required "></label-field>\
+			<div class="input-group input-daterange" data-bind="mvDate: startDate.options, value: startDate.value">\
+				<div class="input-group date" >\
+		    		<input type="text" class="input-sm form-control" name="start" />\
+					<span class="calendar add-on input-group-addon btn" data-bind="attr : { disabled : startDate.disabled }">\
+						<span class="glyphicon glyphicon-calendar"></span>\
+					</span>\
+		        </div>\
+				<span class="input-group-addon to">a</span>\
+				<div class="input-group date">\
+		        	<input type="text" class="input-sm form-control" name="end" data-bind="attr : { disabled : endDate.disabled }" />\
+					<span class="calendar add-on input-group-addon btn" data-bind="attr : { disabled : endDate.disabled }">\
+						<span class="glyphicon glyphicon-calendar"></span>\
+					</span>\
+				</div>\
+		    </div>\
+		</div>'
 });
