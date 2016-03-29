2848b0f82d3650f04bf1fa0351981665beb5495e
Alexandre Araújo <alexandre.goncalves@mv.com.br>
2016-03-29 16:40:46 -0300

    components

--- modulo-server-front/src/main/java/br/com/mv/modulo/components/element/MVDateElementProcessor.java
+++ modulo-server-front/src/main/java/br/com/mv/modulo/components/element/MVDateElementProcessor.java
@@ -1,14 +1,9 @@
 package br.com.mv.modulo.components.element;
 
 import org.thymeleaf.Arguments;
-import org.thymeleaf.Configuration;
 import org.thymeleaf.dom.Element;
-import org.thymeleaf.dom.Text;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.element.AbstractElementProcessor;
-import org.thymeleaf.standard.expression.IStandardExpression;
-import org.thymeleaf.standard.expression.IStandardExpressionParser;
-import org.thymeleaf.standard.expression.StandardExpressions;
 import org.thymeleaf.util.Validate;
 
 public class MVDateElementProcessor extends AbstractElementProcessor {
@@ -31,61 +26,30 @@ public class MVDateElementProcessor extends AbstractElementProcessor {
         super(ELEMENT_NAME);
     }
 	
-    protected final String getText(final Arguments arguments, final String attributeValue) {
-        final Configuration configuration = arguments.getConfiguration();
-        final IStandardExpressionParser expressionParser = StandardExpressions.getExpressionParser(configuration);
-        final IStandardExpression expression = expressionParser.parseExpression(configuration, arguments, attributeValue);
-        final Object result = expression.execute(configuration, arguments);
-        return (result == null ? "" : result.toString());
-    }
 
 	@Override
 	protected ProcessorResult processElement(Arguments arguments, Element element) {
-		String mvAttrId = element.getAttributeValueFromNormalizedName(DIALECT_PREFIX, "id");
 		String mvAttrValue = element.getAttributeValueFromNormalizedName(DIALECT_PREFIX, "value");
-		String mvAttrRequired = element.getAttributeValueFromNormalizedName(DIALECT_PREFIX, "required");
 		String mvAttrDisabled = element.getAttributeValueFromNormalizedName(DIALECT_PREFIX, "disabled");
-		String mvAttrLabel = element.getAttributeValueFromNormalizedName(DIALECT_PREFIX, "label");
-		
-		String mvLabel = mvAttrLabel != null ? getText(arguments, "#{" + mvAttrLabel + "}") : "";
 		
 		Element div = new Element("div");
 		div.setAttribute("class", "date-field date form-group");
 		Validate.notNull(mvAttrValue, "Campo value obrigatório");
 		div.setAttribute("data-bind", "mvDate:{ value: " + mvAttrValue + "}");
 		
-		Element label = new Element("label");
-		label.setAttribute("class", "control-label");
-		if (Boolean.parseBoolean(mvAttrRequired)) {
-			label.setAttribute("class", label.getAttributeValue("class") + " " + "required");
-		}
-		
-		if (mvLabel != null) {
-			label.addChild(new Text(mvLabel));
-		}
-		
-		div.addChild(label);
+		MVLabelElementGenerator mvLabelElementGenerator = new MVLabelElementGenerator(arguments, element);
+		div.addChild(mvLabelElementGenerator.getLabel());
 		
 		Element divInterna = new Element("div");
 		divInterna.setAttribute("class", "input-group input-date-field date-field");
 		
-		Element input = new Element("input");
-		input.setAttribute("type", "text");
-		input.setAttribute("class", "form-control");
-		
-		if (mvAttrId != null) {
-			input.setAttribute("id" , mvAttrId);
-			label.setAttribute("id", mvAttrId + "Label");
-			label.setAttribute("for", mvAttrId);
-		}
-		
-		divInterna.addChild(input);
+		MVInputElementGenerator mvInputElementGenerator = new MVInputElementGenerator(arguments, element);
+		divInterna.addChild(mvInputElementGenerator.getInput());
 		
 		Element span = new Element("span");
 		
 		if (Boolean.parseBoolean(mvAttrDisabled)) {
 			span.setAttribute("disabled", "disabled");
-			input.setAttribute("disabled", "disabled");
 		}
 		
 		span.setAttribute("class", "input-group-addon btn");
